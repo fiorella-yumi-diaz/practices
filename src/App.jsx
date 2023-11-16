d0a2d9a72f24e95a8db354b6b0ce253227cbd6ed
yumi <fiorella.dm2002@gmail.com>
2023-11-16 17:31:00 -0500

    subiendo cambios practica 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 function App() {
 
   return (
-    <div className="bg-purple-300 py-4 px-4">
-      <h1 className="font-bold text-3xl  bg-purple-600 py-2 px-2">practicando Spacing de tailwind css</h1>
-      <p>pading</p>
+    <div className="bg-purple-300 py-4 px-4 flex flex-col h-screen justify-center items-center	">
+      <div className=" h-3/5	w-2/5		bg-purple-600 flex justify-center items-center">
+        <div className="bg-white h-1/4 w-1/4 m-8	"></div>
+        <div className="bg-white h-1/4 w-1/4 m-8	"></div>
+        <div className="basis-1/2 bg-white h-1/4 w-1/4 m-8	"></div>
+      </div>
     </div>
   )
 }
