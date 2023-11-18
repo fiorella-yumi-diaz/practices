3f072a89ea82e6d79abca945a37a0e2d785f1a54
yumi <fiorella.dm2002@gmail.com>
2023-11-17 22:03:41 -0500

    boton de modo oscuro con configuraciones en tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import { useState, useEffect } from "react";
+
 function App() {
+const [theme, setTheme] = useState(() => {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    return "light"
+  }
+  return "dark"
+});
+
+useEffect(() => {
+  if (theme === "dark") {
+    document.querySelector("html").classList.add("dark")
+  } else {
+    document.querySelector("html").classList.remove("dark")
+  }
+},[theme])
+
+const handleChangeTheme = () => {
+  setTheme(prevTheme => prevTheme == "light"? "dark" : "light")
+}
 
   return (
-    <div className="bg-purple-300  py-4 px-4 flex flex-row h-screen flex-wrap		justify-center">
-            <div className=" h-64		w-64	bg-purple-600 m-4	"></div>
-            <div className=" h-64		w-64	bg-purple-600 m-4	"></div>
-            <div className=" h-64		w-64	bg-purple-600 m-4	"></div>
-            <div className=" h-64		w-64	bg-purple-600 m-4	"></div>
-            <div className=" h-64		w-64	bg-purple-600 m-4	"></div>
+    <div className=" bg-white  py-4 px-4 flex flex-row h-screen flex-wrap	items-center dark:bg-black">
+            <button className="px-4 py-2 bg-purple-300 rounded m-4 text-white	hover:bg-purple-400 dark:bg-purple-600 dark:hover:bg-purple-300" 
+            onClick={handleChangeTheme}>Theme changes</button>
+
     </div>
   )
 }
