6f9f842ce60d3f22c63263e92c0816963f69fd8f
yumi <fiorella.dm2002@gmail.com>
2023-11-27 22:37:25 -0500

    Navegador

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from "react";
 function App() {
 const [theme, setTheme] = useState(() => {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    return "light"
+    return "dark"
   }
-  return "dark"
+  return "light"
 });
 
 useEffect(() => {
@@ -21,8 +21,16 @@ const handleChangeTheme = () => {
 }
 
   return (
-    <div className=" bg-white  py-4 px-4 flex flex-row h-screen flex-wrap	items-center dark:bg-black">
-            <button className="px-4 py-2 bg-purple-300 rounded m-4 text-white	hover:bg-purple-400 dark:bg-purple-600 dark:hover:bg-purple-300" 
+    <div className=" bg-white flex flex-col h-screen flex-wrap dark:bg-black">
+      <header className="bg-purple-400 flex flex-row	justify-around items-center	font-medium	dark:text-white dark:bg-purple-900">
+        <h1 className="">Pagina de practicas de tailwindcss</h1>
+        <nav className="flex  ">
+          <div className="px-3	py-3 hover:bg-purple-600 rounded dark:hover:bg-purple-600">Inicio</div>
+          <div className="px-3	py-3 hover:bg-purple-600 rounded dark:hover:bg-purple-600">Herramientas</div>
+          <div className="px-3	py-3 hover:bg-purple-600 rounded dark:hover:bg-purple-600 ">Mas cosas</div>
+        </nav>
+      </header>
+            <button className="px-4 py-2 inline-block bg-purple-300 rounded m-4 text-white	hover:bg-purple-400 dark:bg-purple-600 dark:hover:bg-purple-300" 
             onClick={handleChangeTheme}>Theme changes</button>
 
     </div>
